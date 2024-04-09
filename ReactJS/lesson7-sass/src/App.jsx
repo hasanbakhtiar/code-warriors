a310d4caf890ffd5e6e231f75247fbb72e5459ac
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-04-09 12:20:54 +0400

    sass

---
+++ ReactJS/lesson7-sass/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import SingleCard from './components/SingleCard'
+
+const App = () => {
+  return (
+    <div>
+        <SingleCard />
+        <ol>
+            <li>list text</li>
+            <li>list text</li>
+            <li>list text</li>
+            <li>list text</li>
+            <li>list text</li>
+            <li>list text</li>
+            <li>list text</li>
+            <li>list text</li>
+            <li>list text</li>
+            <li>list text</li>
+            <li>list text</li>
+            <li>list text</li>
+        </ol>
+    </div>
+  )
+}
+
+export default App
---
+++ ReactJS/lesson7-sass/src/components/SingleCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SingleCard = () => {
+  return (
+    <div className='single-card'>
+            <img width={400} src="https://pressupinc.com/wp-content/uploads/2014/03/sass-logo.png" alt="" />
+            <div className="text-con">
+                <h3>Lorem ipsum dolor sit amet.</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sunt harum ea consectetur ipsa ut. Dignissimos rem iusto temporibus hic.</p>
+            </div>
+    </div>
+  )
+}
+
+export default SingleCard
