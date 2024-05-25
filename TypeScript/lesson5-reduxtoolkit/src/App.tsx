af602985a395c3b08cbc62ce43ede444f5055f8b
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-05-25 17:58:53 +0400

    hooks

--- TypeScript/lesson5-reduxtoolkit/src/App.tsx
+++ TypeScript/lesson5-reduxtoolkit/src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import { add } from './features/todoSlice';
+import { useAppDispatch, useAppSelector } from './store';
 
 const App = () => {
-    const data = useSelector((p:any)=>p.todos);
-    const dispatch = useDispatch();
+    const data = useAppSelector((p:any)=>p.todos);
+    const dispatch = useAppDispatch();
   return (
     <div>
         {data.map((item:any)=>(
